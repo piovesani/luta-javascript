2cb6d4672aa4aa47f6d33f47244bd86a8feeb420
Renan Piovesani <renan.piovesani2021@gmail.com>
2023-03-28 20:22:23 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let log = new Log(document.querySelector('.log'));
+
+let char = new Knight('Renan');
+let monster = new LittleMonster();
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster'),
+    log
+);
+
+stage.start();
+
